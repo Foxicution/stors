22f73d7623e5d247e22f5884af5ed68710bae1c1
Foxicution <foxicution@gmail.com>
2025-02-27 21:36:41 +0200

    move server init code to a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,7 @@
-use lsp_server::{Connection, Message, Request, Response};
-use lsp_types::{
-    Hover, HoverContents, HoverParams, HoverProviderCapability, InitializeParams, MarkupContent,
-    MarkupKind, ServerCapabilities, TextDocumentSyncCapability, TextDocumentSyncKind,
-};
-use serde_json::json;
+use server::start_lsp_server;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (connection, io_threads) = Connection::stdio();
+mod server;
 
-    let server_capabilities = serde_json::to_value(ServerCapabilities {
-        text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
-        hover_provider: Some(HoverProviderCapability::Simple(true)),
-        ..Default::default()
-    })?;
-
-    let initialization_params = connection.initialize(server_capabilities)?;
-    let _params: InitializeParams = serde_json::from_value(initialization_params)?;
-
-    for msg in &connection.receiver {
-        match msg {
-            Message::Request(req) if connection.handle_shutdown(&req).unwrap_or(false) => {
-                break;
-            }
-            Message::Request(req) => {
-                if req.method == "textDocument/hover" {
-                    if let Ok(hover_params) = serde_json::from_value::<HoverParams>(req.params) {
-                        let hover_response = Hover {
-                            contents: HoverContents::Markup(MarkupContent {
-                                kind: MarkupKind::Markdown,
-                                value: "This is a **Markdown** hover example.".to_string(),
-                            }),
-                            range: None,
-                        };
-
-                        let response = Response::new_ok(req.id, json!(hover_response));
-                        connection.sender.send(Message::Response(response))?;
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
-
-    io_threads.join()?;
-    Ok(())
+fn main() {
+    start_lsp_server().expect("Starting the lsp server failed!");
 }
